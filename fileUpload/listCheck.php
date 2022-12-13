f7c07281b924ca97fe21855b28816787c93d550f
T19CS006-Ishikawa <takumi13226@192.168.0.11>
2022-12-14 06:19:36 +0900

    echoてづと

--- fileUpload/listCheck.php
+++ fileUpload/listCheck.php
@@ -4,15 +4,17 @@ var_dump($result);
 
 $count = 0;
 $name = array();
+$path = $result[0];
+echo $path;
+/*
 while($count <= count($result) ){
     //echo $result[$count];
     $name[] = $result[$count];
     echo $name[$count];
     echo '<br>';
-    print($name);
-    echo '<br>';
     $count++;
 }
+*/
 
 
 //fclose($handle);
