5dae0c1f1e21f6944edea56a8f4465eab4c50d44
T19CS006-Ishikawa <takumi13226@192.168.0.11>
2022-12-14 05:06:42 +0900

    fopenで確かめる

--- fileUpload/listCheck.php
+++ fileUpload/listCheck.php
@@ -1,4 +1,11 @@
 <?php
 $result = glob('./upfile/');
-var_dump($result);
+//var_dump($result);
+$fname = $result[0];
+$handle = fopen($fname,"r");
+while($line = fgets($handle)){
+    echo $line;
+}
+fclose($handle);
+
   ?>
