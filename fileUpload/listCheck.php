b6f799e6d14571a18bd965ce7f931c2a39106e20
T19CS006-Ishikawa <takumi13226@192.168.0.11>
2022-12-14 06:13:14 +0900

    edit

--- fileUpload/listCheck.php
+++ fileUpload/listCheck.php
@@ -5,9 +5,10 @@ var_dump($result);
 $count = 0;
 $name = array();
 while($count <= count($result) ){
-    //echo$result[$count];
+    //echo $result[$count];
     $name[] = $result[$count];
     echo $name[$count];
+    echo '<br>';
     print($name);
     echo '<br>';
     $count++;
