49164b81cf9056f63094a4d98c5b5947a5699ffe
T19CS006-Ishikawa <takumi13226@192.168.0.6>
2023-01-17 17:08:20 +0900

    edit

--- fileUpload/getFileName.php
+++ fileUpload/getFileName.php
@@ -1,33 +1,10 @@
 
 <?php
-//呼び出されるPHP(ex. getPath.php)
-//$dir = @opendir('/upfile');
-
-$result = glob('./upfile/*');
-
-//var_dump($result);
-
 
 $print = fopen("./upfile/list.txt","r");
 $name = fgets($print);
 echo $name;
-/*
-while($line = fgets($print)){
-    echo $line;
-}
-fclose($print);
-*/
 
-/*
-$count = 0;
-
-while($count <= count($result) ){
-    echo $result[$count];
-    echo '<br>';
-    
-    $count++;
-}
-*/
 ?>
 
 
